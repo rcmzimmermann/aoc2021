85d9c90623c722db796fb1df7b0ba1ccf25d844a
Oddyshh <53621671+Oddyshh@users.noreply.github.com>
2022-12-03 13:56:21 +0100

    2022 day three

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,32 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
+    final String ROCK = "Rock";
+    final String PAPER = "Paper";
+    final String SCISSORS = "Scissors";
+    final String LOSE = "Lose";
+    final String DRAW = "Draw";
+    final String WIN = "WIN";
+
     public static void main(String[] args) {
         Main main = new Main();
+
+
+
         int resultPartOne = 0;
         int resultPartTwo = 0;
         try {
-//            resultPartOne = main.resultDayOnePartOne(main.getIntegerInputFromList("day-one.txt"));
-//            resultPartTwo = main.resultDayOnePartTwo(main.getIntegerInputFromList("day-one.txt"));
-//            resultPartOne = main.resultDayTwoPartOne(main.getStringInputFromList("day-two.txt"));
-//            resultPartTwo = main.resultDayTwoPartTwo(main.getStringInputFromList("day-two.txt"));
-            resultPartOne = main.resultDayThreePartOne(main.getStringInputFromList("day-three.txt"));
-            resultPartTwo = main.resultDayThreePartTwo(main.getStringInputFromList("day-three.txt"));
+            resultPartOne = main.resultDayThreePartTwo(main.getStringInputFromList("C:\\Users\\Robii\\Documents\\Programming\\aoc2021\\day-three.txt"));
+
         } catch (FileNotFoundException e) {
             System.out.println("File not found!");
             e.printStackTrace();
         }
 
         System.out.println("Result 1: " + resultPartOne);
-        System.out.println("Result 2: " + resultPartTwo);
     }
 
     public ArrayList<Integer> getIntegerInputFromList(String fileName) throws FileNotFoundException {
@@ -31,7 +35,11 @@ public class Main {
         Scanner scanner = new Scanner(file);
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
-            inputList.add(Integer.valueOf(line));
+            if (line.equals("")) {
+                inputList.add(null);
+            } else {
+                inputList.add(Integer.valueOf(line));
+            }
         }
         scanner.close();
         return inputList;
@@ -49,179 +57,183 @@ public class Main {
         return inputList;
     }
 
-    public int resultDayOnePartOne(ArrayList<Integer> inputList) {
-        int counter = 0;
-        for (int i = 1; i < inputList.size(); i++) {
-            if (inputList.get(i) > inputList.get(i - 1)) {
-                counter++;
+    public int resultDayOne(ArrayList<Integer> inputList) {
+        List<Integer> list = new ArrayList<>();
+        int totalCalsPerElf = 0;
+
+        for (Integer integer : inputList) {
+            if (integer != null) {
+                totalCalsPerElf += integer;
+            } else {
+                list.add(totalCalsPerElf);
+                totalCalsPerElf = 0;
             }
         }
-        return counter;
+
+        Collections.sort(list);
+        System.out.println(list);
+
+        int top3Cals = list.get(list.size()-1) + list.get(list.size()-2) + list.get(list.size()-3);
+        System.out.println(top3Cals);
+
+        return 0;
     }
 
-    public int resultDayOnePartTwo(ArrayList<Integer> inputList) {
-        int counter = 0;
-        int restWaarde = (inputList.size() % 3) + 1;
-        for (int i = 0; i < inputList.size() - restWaarde; i++) {
-            int sum1 = inputList.get(i) + inputList.get(i + 1) + inputList.get(i + 2);
-            int sum2 = inputList.get(i + 1) + inputList.get(i + 2) + inputList.get(i + 3);
-            if (sum1 < sum2) {
-                counter++;
+    public int resultDayTwo(ArrayList<String> inputList) {
+        Map<String, Integer> scoreMap = new HashMap<>();
+        scoreMap.put(ROCK, 1);
+        scoreMap.put(PAPER, 2);
+        scoreMap.put(SCISSORS, 3);
+
+        List<String> newMoves = new ArrayList<>();
+        List<Integer> choiceScore = new ArrayList<>();
+        List<Integer> resultScore = new ArrayList<>();
+
+        for (String move : inputList) {
+            move = move.replace("A", ROCK);
+            move = move.replace("B", PAPER);
+            move = move.replace("C", SCISSORS);
+            move = move.replace("X", LOSE);
+            move = move.replace("Y", DRAW);
+            move = move.replace("Z", WIN);
+
+            String otherChoice = move.split(" ")[0];
+            newMoves.add(otherChoice + " " + getRightMove(move));
+        }
+
+        for (String move : newMoves) {
+            String otherChoice = move.split(" ")[0];
+            String myChoice = move.split(" ")[1];
+
+            choiceScore.add(scoreMap.get(myChoice));
+
+            if (myChoice.equals(otherChoice)) {
+                resultScore.add(3);
+            } else if (myChoice.equals(ROCK) && otherChoice.equals(PAPER)) {
+                resultScore.add(0);
+            } else if (myChoice.equals(ROCK) && otherChoice.equals(SCISSORS)) {
+                resultScore.add(6);
+            } else if (myChoice.equals(PAPER) && otherChoice.equals(ROCK)) {
+                resultScore.add(6);
+            } else if (myChoice.equals(PAPER) && otherChoice.equals(SCISSORS)) {
+                resultScore.add(0);
+            } else if (myChoice.equals(SCISSORS) && otherChoice.equals(ROCK)) {
+                resultScore.add(0);
+            } else if (myChoice.equals(SCISSORS) && otherChoice.equals(PAPER)) {
+                resultScore.add(6);
             }
         }
-        return counter;
+
+        int totalScore = 0;
+        for (int i = 0; i < choiceScore.size(); i++) {
+            totalScore += choiceScore.get(i);
+            totalScore += resultScore.get(i);
+        }
+
+        System.out.println(choiceScore);
+
+        return totalScore;
     }
 
-    public int resultDayTwoPartOne(ArrayList<String> inputList) {
-        String[] array;
-        int horizontal = 0;
-        int depth = 0;
-        for (String s : inputList) {
-            array = s.split(" ");
-            switch (array[0]) {
-                case "forward":
-                    horizontal += Integer.parseInt(array[1]);
-                    break;
-                case "down":
-                    depth += Integer.parseInt(array[1]);
-                    break;
-                case "up":
-                    depth -= Integer.parseInt(array[1]);
-                    break;
-                default:
-                    System.out.println("Can only go forward, down or up!");
-            }
-        }
-        return horizontal * depth;
-    }
+    private String getRightMove(String input) {
+        String rightChoice = input.split(" ")[1];
+        String otherChoice = input.split(" ")[0];
 
-    public int resultDayTwoPartTwo(ArrayList<String> inputList) {
-        String[] array;
-        int aim = 0;
-        int horizontal = 0;
-        int depth = 0;
-        for (String s : inputList) {
-            array = s.split(" ");
-            switch (array[0]) {
-                case "forward":
-                    horizontal += Integer.parseInt(array[1]);
-                    if (aim != 0) {
-                        depth += Integer.parseInt(array[1]) * aim;
-                    }
-                    break;
-                case "down":
-                    aim += Integer.parseInt(array[1]);
-
-                    break;
-                case "up":
-                    aim -= Integer.parseInt(array[1]);
-                    break;
-                default:
-                    System.out.println("Can only go forward, down or up!");
-            }
+        switch (rightChoice) {
+            case DRAW:
+                if (otherChoice.equals(ROCK)) return ROCK;
+                if (otherChoice.equals(PAPER)) return PAPER;
+                if (otherChoice.equals(SCISSORS)) return SCISSORS;
+            case LOSE:
+                if (otherChoice.equals(ROCK)) return SCISSORS;
+                if (otherChoice.equals(PAPER)) return ROCK;
+                if (otherChoice.equals(SCISSORS)) return PAPER;
+            case WIN:
+                if (otherChoice.equals(ROCK)) return PAPER;
+                if (otherChoice.equals(PAPER)) return SCISSORS;
+                if (otherChoice.equals(SCISSORS)) return ROCK;
+            default:
+                return null;
         }
-        return horizontal * depth;
     }
 
     public int resultDayThreePartOne(ArrayList<String> inputList) {
-        int numInputs = inputList.size();
-        int[] mostCommon = new int[inputList.get(0).length()];
-        for (String string : inputList) {
-            for (int j = 0; j < string.length(); j++) {
-                if (string.charAt(j) == '1') {
-                    mostCommon[j]++;
-                }
-            }
+        String letters = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
+        List<String> list = Arrays.asList(letters.split(","));
+        int totalScore = 0;
+        for (int i = 0; i < inputList.size(); i++) {
+            String[] subString = getSubString(inputList.get(i));
+            String doubleChar = getCharInBothStrings(subString);
+            totalScore += (list.indexOf(doubleChar) + 1);
         }
-
-        String gammaStr = "";
-        String epsilonStr = "";
-        for (int i : mostCommon) {
-
-            if (i > numInputs / 2) {
-                gammaStr += 1;
-                epsilonStr += 0;
-            } else {
-                gammaStr += 0;
-                epsilonStr += 1;
-            }
-        }
-
-        int gammaRate = Integer.parseInt(gammaStr, 2);
-        int epsilonRate = Integer.parseInt(epsilonStr, 2);
-
-        return gammaRate * epsilonRate;
+        return totalScore;
     }
 
     public int resultDayThreePartTwo(ArrayList<String> inputList) {
-        ArrayList<String> o2List = new ArrayList<>();
-        ArrayList<String> co2List = new ArrayList<>();
-
-        for (String string : inputList) {
-            o2List.add(string);
-            co2List.add(string);
+        String letters = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
+        List<String> letterlist = Arrays.asList(letters.split(","));
+        int totalScore = 0;
+        List<String> list = getGroupsOfThree(inputList);
+        for (int i = 0; i < list.size(); i++) {
+            String[] array = list.get(i).split(";");
+            String tripleChar = getCharInThreeStrings(array);
+            totalScore += (letterlist.indexOf(tripleChar) + 1);
         }
-
-        int o2 = 0;
-        int co2 = 0;
-
-        for (int i = 0; i < inputList.get(0).length(); i++) {
-            if (o2List.size() > 1) {
-                o2List = getNewO2InputList(o2List, i);
-            }
-        }
-
-        for (int i = 0; i < inputList.get(0).length(); i++) {
-            if (co2List.size() > 1) {
-                co2List = getNewCO2InputList(co2List, i);
-            }
-        }
-
-        if (!o2List.isEmpty() && !co2List.isEmpty()) {
-            o2 = Integer.parseInt(o2List.get(0), 2);
-            co2 = Integer.parseInt(co2List.get(0), 2);
-        }
-
-        return o2 * co2;
-
+        return totalScore;
     }
 
-    public ArrayList<String> getNewO2InputList(ArrayList<String> inputList, int stringIndex) {
-        int numOne = 0;
-
-        for (String string : inputList) {
-            if (string.charAt(stringIndex) == '1') {
-                numOne++;
-            }
-        }
-
-        char mostCommon;
-        mostCommon = numOne >= (inputList.size() / 2 + inputList.size() % 2) ? '1' : '0';
-
-        for (int i = 0; i < inputList.size(); i++) {
-            inputList.removeIf(string -> string.charAt(stringIndex) != mostCommon);
-        }
-
-        return inputList;
+    private String[] getSubString(String string) {
+        int size = string.length()/2;
+        StringBuilder sb = new StringBuilder(string);
+        sb.insert(size, " ");
+        String[] array = sb.toString().split(" ");
+        return array;
     }
 
-    public ArrayList<String> getNewCO2InputList(ArrayList<String> inputList, int stringIndex) {
-        int numOne = 0;
+    private String getCharInBothStrings(String[] array) {
+        String one = array[0];
+        String two = array[1];
 
-        for (String string : inputList) {
-            if (string.charAt(stringIndex) == '1') {
-                numOne++;
+        for (int i = 0; i < one.length(); i++) {
+            String chr = String.valueOf(one.charAt(i));
+            if (two.contains(chr)) {
+                return chr;
             }
         }
+        return null;
+    }
 
-        char lessCommon;
-        lessCommon = numOne >= (inputList.size() / 2 + inputList.size() % 2) ? '0' : '1';
+    private String getCharInThreeStrings(String[] array) {
+        String one = array[0];
+        String two = array[1];
+        String three = array[2];
 
-        for (int i = 0; i < inputList.size(); i++) {
-            inputList.removeIf(string -> string.charAt(stringIndex) != lessCommon);
+        for (int i = 0; i < one.length(); i++) {
+            String chr = String.valueOf(one.charAt(i));
+            if (two.contains(chr) && three.contains(chr)) {
+                System.out.println(chr);
+                return chr;
+            }
         }
+        return null;
+    }
 
-        return inputList;
+    private List<String> getGroupsOfThree(ArrayList<String> inputList) {
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            String groupString = inputList.remove(0) + ";" + inputList.remove(0) + ";" + inputList.remove(0);
+
+            list.add(getLongest(groupString));
+        }
+        return list;
+    }
+
+    private String getLongest(String str) {
+        Comparator c = (Comparator<String>) (s1, s2) -> Integer.compare(s2.length(), s1.length());
+        List<String> list = Arrays.asList(str.split(";"));
+        Collections.sort(list, c);
+
+        return list.get(0) + ";" + list.get(1) + ";" + list.get(2);
     }
 
 }
